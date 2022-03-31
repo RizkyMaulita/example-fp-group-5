f49ed9507aeafb4004d6de85ac16353fe926042e
Rizky Maulita <56714451+RizkyMaulita@users.noreply.github.com>
2022-03-31 19:38:11 +0700

    initial commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,43 @@
+import { Form, Button, Container } from "react-bootstrap";
+
 function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <Container>
+      <div className="row row-cols-2">
+        <div>
+          <img
+            src="https://res.cloudinary.com/dxdgvvdwi/image/upload/v1648729302/Ilustration/undraw_my_password_re_ydq7_d1bdf0.svg"
+            width="100%"
+            height="90%"
+            alt="img-login"
+          />
+        </div>
+        <div>
+          <h1 className="text-center mb-3">Register</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default RegisterPage;
